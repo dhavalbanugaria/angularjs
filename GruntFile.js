6d5f04c964e3324b8716091061b1814ceae4eaef
dhavalbanugaria31 <dhavalbanugaria31@gmail.com>
2015-09-14 23:06:11 -0400

    Changes done in Gruntfile, bower.json, package.json

---
+++ GruntFile.js
@@ -0,0 +1,80 @@
+module.exports = function (grunt)
+{
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: "\n\n"
+			},
+			dist: {
+				src: ['src/resources/js/**/*.js'],
+				dest: 'src/<%= pkg.name %>.js'
+			},
+			deps: {
+				src: [
+					'bower_components/modernizr/modernizr.js',
+					'bower_components/jquery/dist/jquery.js',
+					'bower_components/bootstrap/dist/js/bootstrap.js',
+					'bower_components/angularjs/angular.min.js',
+				],
+				dest: 'src/<%= pkg.name %>-deps.js'
+			},
+			css: {
+				src: ['bower_components/bootstrap/dist/css/bootstrap.min.css',
+						'src/resources/css/styles.css'
+				],
+				dest: 'src/resources/css/<%= pkg.name %>.css'
+			},
+			move: {
+				src: ['bower_components/angularjs/angular.min.js.map'],
+				dest: 'src/angular.min.js.map'
+			}
+		},
+
+		sass: {
+			dev: {
+				files: {
+					'src/resources/css/styles.css': 'src/resources/css/styles.scss'
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: ['src/resources/js/**/*.js'],
+				tasks: ['concat:dist']
+			},
+			styles: {
+				files: ['src/resources/css/*.scss'],
+				tasks: ['sass']
+			}
+		},
+
+		serve: {
+	        options: {
+	            port: 9000,
+	            'serve': {
+	                'path': 'src'
+	            }
+	        }
+   		 }, 
+		
+	});
+
+	//npm tasks
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-ngdocs');
+	grunt.loadNpmTasks('grunt-serve');
+
+	//tasks
+	grunt.registerTask('default', 'Default Task Alias', ['build','serve']);
+
+	grunt.registerTask('build', 'Build the application', 
+		['sass:dev',
+		'concat'
+		]);
+}
